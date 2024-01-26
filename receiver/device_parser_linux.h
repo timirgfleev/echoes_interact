91665b132a312c6da8c3f2b2449ef39d3d4bf69e
muta <timirgfleev@gmail.com>
2024-01-26 22:15:21 +0300

    Complete device parser

--- receiver/device_parser_linux.h
+++ receiver/device_parser_linux.h
@@ -1,9 +1,24 @@
+#pragma once
+#include <sys/utsname.h>
+#include <unistd.h>
+#include <sys/sysinfo.h>
+#include <fstream>
+
 #include <string>
 #include <vector>
+
+struct DeviceInfo
+{
+    std::string device_name;
+    std::string os_ver;
+    std::string part_number;
+    std::vector<std::string> device_description;
+};
+
 class DeviceParser
 {
 public:
-    std::vector<std::string> get_device_info()
+    DeviceInfo get_device_info()
     {
         return {get_name(),
                 get_os_ver(),
@@ -14,16 +29,48 @@ public:
 protected:
     std::string get_name()
     {
-        
+        const size_t HOST_NAME_MAX = 100;
+        char hostname[HOST_NAME_MAX];
+        gethostname(hostname, HOST_NAME_MAX);
+        return std::string(hostname);
     }
 
     std::string get_os_ver()
     {
+        struct utsname buffer;
+        uname(&buffer);
+        return std::string(buffer.sysname) + " " + std::string(buffer.release);
     }
-    std::string get_serial_number()
-    {
+
+std::string get_serial_number()
+{
+    std::ifstream ifs("/sys/class/dmi/id/product_serial");
+    std::string serial;
+
+    if (!ifs) {
+        serial = "NO SERIAL";
     }
-    std::string get_description()
+
+    
+    std::getline(ifs, serial);
+    if (ifs.fail()) {
+        serial = "NO SERIAL";
+    }
+
+    return serial;
+}
+
+    std::vector<std::string> get_description()
     {
+        struct sysinfo si;
+        sysinfo(&si);
+        // printf("Uptime : %ld\n", si.uptime);
+        // printf("Free RAM : %ld\n", si.freeram);
+        // printf("Total RAM : %ld\n", si.totalram);
+        // printf("Process count : %d\n", si.procs);
+        return {std::to_string(si.uptime),
+                std::to_string(si.freeram),
+                std::to_string(si.totalram),
+                std::to_string(si.procs)};
     }
 };
