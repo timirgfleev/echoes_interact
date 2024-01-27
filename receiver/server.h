ff1d42069c9cada4d870f19e4782bca9c8eb086e
muta <timirgfleev@gmail.com>
2024-01-27 19:49:09 +0300

    Make use of permission checker, refactor

--- receiver/server.h
+++ receiver/server.h
@@ -6,6 +6,7 @@
 
 #include "device_parser_linux.h"
 
+#include "permission_checker.h"
 using boost::asio::ip::tcp;
 
 class Server
@@ -19,29 +20,30 @@ public:
     void StartReceive();
 
 protected:
-    void message_received_callback(const boost::system::error_code &error, std::size_t bytes_transferred);
-
     void send_callback(const boost::system::error_code &error, std::size_t bytes_transferred);
-
-    void handle_message(coolProtocol::MessageWrapper host_msg);
-
     void send_reply(coolProtocol::MessageWrapper reply_msg);
 
-    void ping_pong();
+    void message_received_callback(const boost::system::error_code &error, std::size_t bytes_transferred);
+    void handle_message(coolProtocol::MessageWrapper host_msg);
+    void process_message(coolProtocol::MessageWrapper host_msg);
 
+    void ping_pong();
     void get_device_info();
 
     void wait_for_reply(u_short deadline = 10);
 
+    // these are error states
+    void persimission_deny();
     void internal_error();
     void bad_data();
-
     void on_timeout();
 
     void close_connection();
-    
+
+private:
+    Permission_Chercker permissions_;
+
     boost::asio::streambuf buffer_;
-    coolProtocol::MessageWrapper expected_;
     tcp::socket sk_;
     boost::asio::deadline_timer timer_;
     bool is_deadline_set_;
