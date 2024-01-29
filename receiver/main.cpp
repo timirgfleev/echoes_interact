c5b4f4ee81bad12266f7a676b97d9fd650440d09
muta <timirgfleev@gmail.com>
2024-01-29 18:38:32 +0300

    Add launch arg options for main

--- receiver/main.cpp
+++ receiver/main.cpp
@@ -4,6 +4,8 @@
 
 using boost::asio::ip::tcp;
 
+typedef boost::asio::ip::port_type Port;
+const Port DEF_PORT = Port(1234);
 
 std::unique_ptr<Server> handle_connection(tcp::socket socket)
 {
@@ -12,7 +14,7 @@ std::unique_ptr<Server> handle_connection(tcp::socket socket)
 }
 
 // main loop
-void listen()
+void listen_for(Port port, size_t exit_after)
 {
     try
     {
@@ -20,13 +22,12 @@ void listen()
         // boost::asio::io_service::work work(io_service)
         // std::thread io_thread([&io_service]() { io_service.run(); });
 
-        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 1234));
-
+        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), port));
 
         std::vector<std::unique_ptr<Server>> v;
         for (;;)
         {
-            
+
             tcp::socket socket(io_service);
             std::cout << "Ready for connections" << std::endl;
             acceptor.accept(socket);
@@ -39,9 +40,13 @@ void listen()
 
             io_service.run();
 
-            static int i = 0;
+            static size_t i = 0;
             std::cout << i++ << "io service dead\n\n"
                       << std::endl;
+            if (i == exit_after)
+            {
+                break;
+            }
         }
     }
     catch (std::exception &e)
@@ -50,11 +55,66 @@ void listen()
     }
 }
 
+void launcher(int argc, char *argv[])
+{
+        try
+    {
+
+        switch (argc)
+        {
+        case 0:
+
+            break;
+        case 2:
+            if (std::string(argv[0]) == "-r")
+            {
+                listen_for(DEF_PORT, 3);
+            }
+            else if (std::string(argv[0]) == "-p")
+            {
+                listen_for(std::stoi(argv[1]), 3);
+            } else if (std::string(argv[0]) == "-e")
+            {
+                listen_for(DEF_PORT, std::stoi(argv[1]));
+            } else {
+                throw std::exception();
+            }
+            break;
+        case 4:
+            if (std::string(argv[0]) == "-p" && std::string(argv[2]) == "-e")
+            {
+                listen_for(std::stoi(argv[1]), std::stoi(argv[3]));
+            }
+            else if (std::string(argv[0]) == "-e" && std::string(argv[2]) == "-p")
+            {
+                listen_for(std::stoi(argv[3]), std::stoi(argv[1]));
+            }
+            else
+            {
+                throw std::exception();
+            }
+            break;
+        default:
+            throw std::exception();
+            break;
+        }
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "USAGE:           [optional] -p <port>\n"
+                  << "                 [optional] -e <number> of connections to accept before exiting\n"
+                  << "[if none optional selected] -r run for 3 connections and on port 1234\n"
+                  << std::endl;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-    listen();
+    launcher(argc, argv);
 
     google::protobuf::ShutdownProtobufLibrary();
+
+    return 0;
 }
