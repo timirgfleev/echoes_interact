29bb08b9b0d7a4eb2c193e0b5bf00b0e8ca86c79
muta <timirgfleev@gmail.com>
2024-01-29 17:29:11 +0300

    Cleanup main

--- receiver/main.cpp
+++ receiver/main.cpp
@@ -4,8 +4,6 @@
 
 using boost::asio::ip::tcp;
 
-// server class.
-// it handles the socket connection when it is established
 
 std::unique_ptr<Server> handle_connection(tcp::socket socket)
 {
@@ -19,22 +17,31 @@ void listen()
     try
     {
         boost::asio::io_service io_service;
-        boost::asio::io_service::work work(io_service);
+        // boost::asio::io_service::work work(io_service)
+        // std::thread io_thread([&io_service]() { io_service.run(); });
 
         tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 1234));
 
-        //std::thread io_thread([&io_service]() { io_service.run(); });
-        
+
+        std::vector<std::unique_ptr<Server>> v;
         for (;;)
         {
+            
             tcp::socket socket(io_service);
             std::cout << "Ready for connections" << std::endl;
             acceptor.accept(socket);
             std::cout << "conn received" << std::endl;
-            std::unique_ptr<Server> srv = handle_connection(std::move(socket));
+            std::unique_ptr<Server> srv =
+                handle_connection(std::move(socket));
             srv->StartReceive();
+            // v.push_back(std::move(srv));
+            // v[v.size() - 1]->StartReceive();
+
             io_service.run();
-            //todo: add thread pool
+
+            static int i = 0;
+            std::cout << i++ << "io service dead\n\n"
+                      << std::endl;
         }
     }
     catch (std::exception &e)
