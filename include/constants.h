67903b0d419cca3b8f46dd8c0389b5b842826683
muta <timirgfleev@gmail.com>
2024-01-29 22:40:36 +0300

    Add comments

--- include/constants.h
+++ include/constants.h
@@ -2,9 +2,13 @@
 #include <cstdint>
 namespace config
 {
+    // as name says
     const int DEFAULT_LOOP_COUNT = 3;
     const int DEF_PORT = 1234;
 
+    // This type used as default timeout in seconds
     const int TIMEOT_LEN = 10;
+
+    // This type describes size message prefix
     typedef uint32_t MESSAGE_SIZE;
 }
