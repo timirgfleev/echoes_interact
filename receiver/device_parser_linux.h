090ea2d4878291062840ab7e6cc85bdaf9f3dcb6
muta <timirgfleev@gmail.com>
2024-01-31 04:18:54 +0300

    Add message creation specific for device parser using fabric

--- receiver/device_parser_linux.h
+++ receiver/device_parser_linux.h
@@ -7,20 +7,24 @@
 #include <string>
 #include <vector>
 
-
-#include "messages.pb.h"
-
+#include "../include/message_creator.h"
+/*
+ * file contain device gathering info code
+ * and message creator for device info
+ */
 class DeviceParser
 {
 public:
-    static coolProtocol::DeviceInfo get_device_info()
+    typedef std::unique_ptr<coolProtocol::DeviceInfo> DeviceInfo_UPTR;
+
+    static DeviceInfo_UPTR get_device_info()
     {
-        coolProtocol::DeviceInfo d;
-        
-        d.set_allocated_name(new std::string(get_name()));
-        d.set_allocated_os_ver(new std::string(get_os_ver()));
-        d.set_allocated_part_number(new std::string(get_serial_number()));
-        assign_description(d);
+        auto d = std::make_unique<coolProtocol::DeviceInfo>();
+
+        d->set_allocated_name(new std::string(get_name()));
+        d->set_allocated_os_ver(new std::string(get_os_ver()));
+        d->set_allocated_part_number(new std::string(get_serial_number()));
+        assign_description(*d);
 
         return d;
     }
@@ -28,7 +32,7 @@ public:
 protected:
     static std::string get_name()
     {
-        //const size_t HOST_NAME_MAX = 100;
+        // const size_t HOST_NAME_MAX = 100;
         char hostname[HOST_NAME_MAX];
         gethostname(hostname, HOST_NAME_MAX);
         return std::string(hostname);
@@ -46,7 +50,7 @@ protected:
         std::ifstream ifs("/sys/class/dmi/id/product_serial");
         std::string serial;
 
-        //of course I could to just not set it in message, but that one I like more
+        // of course I could to just not set it in message, but that one I like more
         if (!ifs)
         {
             serial = "NO SERIAL";
@@ -61,7 +65,7 @@ protected:
         return serial;
     }
 
-    static void assign_description(coolProtocol::DeviceInfo& d)
+    static void assign_description(coolProtocol::DeviceInfo &d)
     {
         struct sysinfo si;
         sysinfo(&si);
@@ -76,3 +80,17 @@ protected:
         d.add_desc(std::to_string(si.procs));
     }
 };
+
+namespace msgCreators
+{
+    class DeviceInfo : public Message
+    {
+    public:
+        Message_UPTR create() override
+        {
+            auto msg = std::make_unique<coolProtocol::MessageWrapper>();
+            msg->set_allocated_device_data(DeviceParser::get_device_info().release());
+            return msg;
+        }
+    };
+}
